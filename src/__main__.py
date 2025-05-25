9f9602c22f0e1d8387569ae3e1cad68470ec0282
Roman A <121314722+GameRoMan@users.noreply.github.com>
2025-05-25 11:33:43 +0100

    Add logging

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,9 @@
 import os
 import requests
+
 from typing import TypedDict
+
+import logging
 
 from storage.database_manager import ElementsDatabaseManager, RecipesDatabaseManager
 
@@ -10,6 +13,17 @@ from temp.recipes import Recipe
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(SRC_DIR)
 DB_PATH = os.path.join(BASE_DIR, "storage", "elements.db")
+LOG_FILE_PATH = os.path.join(BASE_DIR, "logs", "app.log")
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)-7s %(message)s",
+    datefmt="%d.%m.%Y %H:%M:%S",
+    handlers=[logging.StreamHandler(), logging.FileHandler(LOG_FILE_PATH, encoding="utf-8")],
+)
+
+logger = logging.getLogger(__name__)
 
 
 class ApiResponse(TypedDict):
@@ -22,49 +36,65 @@ def fetch_recipes(element_db: str) -> ApiResponse:
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Error fetching recipes for element ID {element_db}: {response.status_code}")
+        logger.error(
+            f"Error fetching recipes for element {element_db}: [{response.status_code}] {response.reason}"
+        )
         return {"recipes": []}
 
 
-elements_db_manager = ElementsDatabaseManager(DB_PATH)
-recipes_db_manager = RecipesDatabaseManager(DB_PATH)
+def main():
+    elements_db_manager = ElementsDatabaseManager(DB_PATH)
+    recipes_db_manager = RecipesDatabaseManager(DB_PATH)
 
-all_elements = elements_db_manager.get_all_elements(offset=5000, limit=25000)
+    all_elements = elements_db_manager.get_all_elements(offset=12_500, limit=500)
 
-for element in all_elements:
-    element_db = element[0]
-    recipes_data = fetch_recipes(element_db)
-    recipes = recipes_data.get("recipes", [])
+    for element in all_elements:
+        element_db = element[0]
+        recipes_data = fetch_recipes(element_db)
+        recipes = recipes_data.get("recipes", [])
+        if not recipes:
+            continue
 
-    recipes_to_add: list[tuple[str, str, str]] = []
-    for recipe_pair in recipes:
-        try:
-            first_element = recipe_pair[0]
-            second_element = recipe_pair[1]
+        recipes_to_add: list[tuple[str, str, str]] = []
+        missing_elements: list[tuple[str, str]] = []
 
-            first_element_db = elements_db_manager.get_element(first_element["id"])
-            second_element_db = elements_db_manager.get_element(second_element["id"])
-            result_element_name = element[1]  # element[1] is the name of the element
-
-            if not first_element_db:
-                elements_db_manager.add_element(first_element["id"], first_element["emoji"])
+        for recipe_pair in recipes:
+            try:
+                first_element = recipe_pair[0]
                 first_element_db = elements_db_manager.get_element(first_element["id"])
-                print(f"Added missing element: {first_element["id"]}")
-            if not second_element_db:
-                elements_db_manager.add_element(second_element["id"], second_element["emoji"])
+
+                if not first_element_db:
+                    missing_elements.append((first_element["id"], first_element["emoji"]))
+
+                second_element = recipe_pair[1]
                 second_element_db = elements_db_manager.get_element(second_element["id"])
-                print(f"Added missing element: {second_element["id"]}")
 
-            if first_element_db and second_element_db:
-                recipes_to_add.append((first_element_db[0], second_element_db[0], result_element_name))
-            else:
-                print(f"Skipping recipe due to missing element: {recipe_pair}")
+                if not second_element_db:
+                    missing_elements.append((second_element["id"], second_element["emoji"]))
+
+                result_element_name = element[1]  # element[1] is the name of the element
+
+                recipes_to_add.append((first_element["id"], second_element["id"], result_element_name))
+
+            except Exception as e:
+                logger.error(f"Error processing recipe: {recipe_pair}. Error: {e}")
+
+        # remove duplicates
+        missing_elements = list(set(missing_elements))
+
+        if missing_elements:
+            try:
+                elements_db_manager.bulk_add_elements(missing_elements)
+                logger.info("Successfully added missing elements")
+            except Exception as e:
+                logger.error(f"Error adding missing elements: {e}")
+
+        try:
+            recipes_db_manager.bulk_add_recipes(recipes_to_add)
+            logger.info("Successfully added recipes")
         except Exception as e:
-            print(f"Error processing recipe: {recipe_pair}. Error: {e}")
+            logger.error(f"Error adding recipes to database: {e}")
 
-    try:
-        recipes_db_manager.bulk_add_recipes(recipes_to_add)
-    except Exception as e:
-        print(f"Error adding recipes to database: {e}")
-    else:
-        print("Successfully added recipes")
+
+if __name__ == "__main__":
+    main()
